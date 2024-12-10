72e09e6c34369f2eabf07809f30da068df942867
Aleksei Zaitsev <alexej3ajtsev@gmail.com>
2024-12-10 21:26:29 +0300

    feat: initial

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("config.lazy")
