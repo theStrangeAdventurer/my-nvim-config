03121fc7f4efdbeac90257570ef7cc8a03e3c71d
Aleksei Zaitsev <alexej3ajtsev@gmail.com>
2024-12-12 21:44:32 +0300

    feat: added buffers navigation

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -1,14 +1,19 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    opts = {
-	window = {
-		mappings = {
-			["l"] = "open",
-			["h"] = "toggle_node",
-		}
-	}
-    },
+    config = function() 
+	print "hi from neo tree"
+	vim.keymap.set('n', '<leader>b', '<cmd>Neotree float buffers<CR>')
+
+	require("neo-tree").setup({
+ 		window = {
+			mappings = {
+				["l"] = "open",
+				["h"] = "toggle_node",
+			}
+		}     
+	})
+    end,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
