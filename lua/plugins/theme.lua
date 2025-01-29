d1d94d4a2d503917b6cea3f079f2cae0fb1f1b93
Alexey Zaytsev <alexej3ajtsev@gmail.com>
2025-01-30 00:17:07 +0500

    chore: removed unused themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -10,23 +10,3 @@ return {
 		require("kanagawa").load "wave"
 	end,
 }
-
--- OTHER COOL THEMES:
--- https://github.com/rmehri01/onenord.nvim
--- return {
--- 	"rmehri01/onenord.nvim",
--- 	opts = {
--- 		theme = 'dark',
--- 		borders = true,
--- 		-- disable = { background = true },
--- 		fade_nc = true
--- 	}
--- }
-
--- return {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
--- 	config = function()
--- 		vim.cmd("colorscheme rose-pine")
--- 	end
--- }
