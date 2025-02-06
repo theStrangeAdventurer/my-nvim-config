660fb7097c6dae39fdc988fd0dc59accdc405943
Alexey Zaytsev <alexej3ajtsev@gmail.com>
2025-02-06 09:17:44 +0500

    fix: add telescope files exclusion

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,32 @@ return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
 	-- or                              , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = { 'nvim-lua/plenary.nvim', {
+		-- https://github.com/princejoogie/dir-telescope.nvim
+		"princejoogie/dir-telescope.nvim",
+		opts = {
+			hidden = true,
+			no_ignore = true,
+			show_preview = true,
+			follow_symlinks = false,
+		}
+	} },
 	config = function()
-		vim.keymap.set("n", "<leader>ff", function()
+		require('telescope').setup { defaults = {
+			file_ignore_patterns = { "node_modules", ".git" }
+		} }
+		vim.keymap.set("n", "<leader>fi", function()
 			vim.cmd('Telescope live_grep')
-		end, { desc = "[C]close current buffer" })
+		end, { desc = "Find [i]nside files" })
+		vim.keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<CR>", {
+			noremap = true,
+			silent = true,
+			desc = "Find in [d]irectory"
+		})
+		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope dir find_files<CR>", {
+			noremap = true,
+			silent = true,
+			desc = "Find [f]iles"
+		})
 	end
 }
