5bbef6a275a60e6643b3b9dca7f22311db330f10
Alexey Zaytsev <alexej3ajtsev@gmail.com>
2025-06-04 11:30:38 +0300

    fix: added telescope ignore files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,22 +3,39 @@ return {
 	tag = '0.1.8',
 	lazy = true,
 	-- or                              , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim', {
-		-- https://github.com/princejoogie/dir-telescope.nvim
-		"princejoogie/dir-telescope.nvim",
-		opts = {
-			hidden = true,
-			no_ignore = true,
-			show_preview = true,
-			follow_symlinks = false,
-		}
-	} },
+	dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', enabled = true } },
 	config = function()
-		require('telescope').setup { defaults = {
-			file_ignore_patterns = { "pnpm-lock.yaml", "package-lock.json", "dist/", "build/", "node_modules", ".git" }
-		} }
+		require('telescope').setup {
+			defaults = {
+				vimgrep_arguments = {
+					'rg',
+					'--color=never',
+					'--no-heading',
+					'--with-filename',
+					'--line-number',
+					'--column',
+					'--smart-case',
+					'--hidden',
+					'--glob=!pnpm-lock.yaml',
+					'--glob=!dist/*',
+					'--glob=!.next/*',
+					'--glob=!build/*',
+					'--glob=!node_modules/*',
+				},
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,    -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				}
+			},
+		}
+		require('telescope').load_extension('fzf')
 		vim.keymap.set("n", "<leader>fi", function()
-			vim.cmd('Telescope live_grep')
+			require("telescope.builtin").live_grep({ hidden = true })
 		end, { desc = "Find [i]nside files" })
 		-- Telescope find_files
 		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {
@@ -30,11 +47,6 @@ return {
 			noremap = true,
 			silent = true,
 			desc = "Find in [d]irectory"
-		})
-		vim.keymap.set("n", "<leader>fd", "<cmd>Telescope dir find_files<CR>", {
-			noremap = true,
-			silent = true,
-			desc = "Find [d]irs"
 		})
 	end
 }
